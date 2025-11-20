972eca4fdbbfceafa69402b3c72f4f6a0ea80d55
Naveen Jothi <naveenjothi040@psnacet.edu.in>
2025-11-20 23:07:17 +0530

    feat: Implement Firebase ID token verification, user lookup, and suspension check, adding an `IsSuspended` field to the base model.

--- pkg/models/base_model.go
+++ pkg/models/base_model.go
@@ -8,10 +8,11 @@ import (
 )
 
 type BaseModel struct {
-	ID        string          `json:"id" gorm:"type:uuid;primary_key;"`
-	CreatedAt time.Time       `json:"created_at"`
-	UpdatedAt time.Time       `json:"updated_at"`
-	DeletedAt *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	ID          string          `json:"id" gorm:"type:uuid;primary_key;"`
+	CreatedAt   time.Time       `json:"created_at"`
+	UpdatedAt   time.Time       `json:"updated_at"`
+	DeletedAt   *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	IsSuspended bool            `json:"is_suspended"`
 }
 
 func (u *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
